6991928a625a783249decdfd4a78f891ae136c2a
Kohei Yamaguchi <fix7211@gmail.com>
2020-03-18 21:19:25 +0900

    Update a header of arp

--- ttcp/arp.h
+++ ttcp/arp.h
@@ -1,12 +1,18 @@
 #ifndef __ARP_H_
 #define __ARP_H_
 
+#include <stdio.h>
+#include "net.h"
 #include "ethernet.h"
 #include "ip.h"
 
-extern void arp_init(void);
-extern int arp_table_lookup(const ip_addr_t *pa, ethernet_addr_t *ha);
-extern void arp_recv(uint8_t *packet, ssize_t plen, ethernet_addr_t *src, ethernet_addr_t *dst);
-extern int arp_send_garp(void);
+#define ARP_RESOLVE_ERROR -1
+#define ARP_RESOLVE_QUERY 0
+#define ARP_RESOLVE_FOUND 1
+
+extern int
+arp_init(void);
+extern int
+arp_resolve(struct netif *netif, const ip_addr_t *pa, uint8_t *ha, const void *data, size_t len);
 
 #endif
