79d9d6a40982f322cb77737a7057131d57bfa588
Kohei Yamaguchi <fix7211@gmail.com>
2020-05-04 22:42:55 +0900

    Update ethernet test

--- test/ethernet_test.c
+++ test/ethernet_test.c
@@ -1,50 +1,52 @@
 #include <stdio.h>
 #include <string.h>
 #include <signal.h>
-#include "device.h"
+#include "raw.h"
+#include "net.h"
 #include "ethernet.h"
-#include "util.h"
 
-
-static void arp_recv(uint8_t *packet, size_t plen, ethernet_addr_t *src, ethernet_addr_t *dst) {
-    char ss[ETHERNET_ADDR_STR_LEN + 1], ds[ETHERNET_ADDR_STR_LEN + 1];
-
-    frpintf(stderr, "%s > %s ARP length: %lu\n",
-            ethernet_addr_ntop(src, ss, sizeof(ss)), ethernet_addr_ntop(dst, ds, sizeof(ds)), plen);
-    hexdump(stderr, packet, plen);
-}
-
-static void ip_recv(uint8_t *dgram, size_t dlen, ethernet_addr_t *src, ethernet_addr_t *dst) {
-    char ss[ETHERNET_ADDR_STR_LEN + 1], ds[ETHERNET_ADDR_STR_LEN + 1];
-
-    fprintf(stderr, "%s > %s IP length: %lu\n",
-            ethernet_addr_ntop(src, ss, sizeof(ss)), ethernet_addr_ntop(dst, ds sizeof(ds)), dlen);
-    hexdump(stderr, dgram, dlen);
+static int setup(void)
+{
+    if (ethernet_init() == -1) {
+        fprintf(stderr, "Initialize ethernet is failure\n");
+        return -1;
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
     sigset_t sigset;
     int signo;
+    struct netdev *dev;
 
-    if (argc != 3) {
-        fprintf(stderr, "usage: %s device-name ethernet-addr\n", argv[0]);
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s interface\n", argv[0]);
         return -1;
     }
+
     sigemptyset(&sigset);
     sigaddset(&sigset, SIGINT);
     sigprocmask(SIG_BLOCK, &sigset, NULL);
-    ethernet_init();
-    if (ethernet_set_addr(argv[2]) == -1) {
-        fprintf(stderr, "error: ethernet-addr is invalid\n");
+
+    if (setup() == -1)
         return -1;
-    }
-    ethernet_add_handler(ETHERNET_TYPE_ARP, arp_recv);
-    ethernet_add_handler(ETHERNET_TYPE_IP, ip_recv);
-    if (device_init(argv[1], ethernet_recv) == -1) {
-        device_cleanup();
+
+    dev = netdev_alloc(NETDEV_TYPE_ETHERNET);
+    if (!dev)
         return -1;
+
+    strncpy(dev->name, argv[1], sizeof(dev->name) - 1);
+    if (dev->ops->open(dev, RAWDEV_TYPE_AUTO) == -1)
+        return -1;
+
+    dev->ops->run(dev);
+    while (1) {
+        sigwait(&sigset, &signo);
+        if (signo == SIGINT)
+            break;
     }
-    sigwait(&sigset, &signo);
-    device_cleanup();
+    if (dev->ops->close)
+        dev->ops->close(dev);
+
     return 0;
 }
